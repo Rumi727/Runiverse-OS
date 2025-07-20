b88c55c59e30fcc2c10efdd3212d1ff84b024792
Rumi <jun32820@gmail.com>
2025-07-20 16:43:48 +0900

    Update EditorLanguageConfigAsset.cs

--- Packages/com.rumi.runiengine.core/Editor/Localizations/EditorLanguageConfigAsset.cs
+++ Packages/com.rumi.runiengine.core/Editor/Localizations/EditorLanguageConfigAsset.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace RuniEngine.Editor.Localizations
 {
-    public sealed class EditorLanguageConfigAsset : ConfigObject<EditorLanguageConfigAsset>
+    public sealed class EditorLanguageConfigAsset : RuniOSConfigObject<EditorLanguageConfigAsset>
     {
         public static string currentLanguage => instance._currentLanguage;
         [SerializeField] string _currentLanguage = "en_us";
