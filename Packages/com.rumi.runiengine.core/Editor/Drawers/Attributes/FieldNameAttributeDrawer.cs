10415f2293a40c3e34e5ada5b7457b5ae597ac55
Rumi <jun32820@gmail.com>
2025-07-20 17:00:55 +0900

    force 프로퍼티 추가

--- Packages/com.rumi.runiengine.core/Editor/Drawers/Attributes/FieldNameAttributeDrawer.cs
+++ Packages/com.rumi.runiengine.core/Editor/Drawers/Attributes/FieldNameAttributeDrawer.cs
@@ -11,11 +11,10 @@ namespace RuniEngine.Editor.Drawers.Attributes
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            if (L10n.Tr(property.displayName) == label.text)
+            FieldNameAttribute attribute = (FieldNameAttribute)this.attribute;
+            if (attribute.force || L10n.Tr(property.displayName) == label.text)
             {
-                FieldNameAttribute attribute = (FieldNameAttribute)this.attribute;
                 label.text = TryGetText(attribute.name);
-
                 EditorGUI.PropertyField(position, property, label, property.IsChildrenIncluded());
             }
             else
--- Packages/com.rumi.runiengine.core/Runtime/Utility/Attribute/FieldNameAttribute.cs
+++ Packages/com.rumi.runiengine.core/Runtime/Utility/Attribute/FieldNameAttribute.cs
@@ -5,8 +5,13 @@ namespace RuniEngine
 {
     public sealed class FieldNameAttribute : PropertyAttribute
     {
-        public FieldNameAttribute(string name) => this.name = name;
+        public FieldNameAttribute(string name, bool force = false)
+        {
+            this.name = name;
+            this.force = force;
+        }
 
         public string name { get; } = "";
+        public bool force { get; } = false;
     }
 }
