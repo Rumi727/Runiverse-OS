ced3edf2682abe299d66cef7275d5836e33eab44
Rumi <jun32820@gmail.com>
2025-07-24 20:20:11 +0900

    Create SerializableNullable.cs

---
+++ Packages/com.rumi.runiengine.core/Runtime/SerializableNullable.cs
@@ -0,0 +1,26 @@
+#nullable enable
+using System;
+
+namespace RuniEngine
+{
+    public static class SerializableNullable
+    {
+        public static Type? GetUnderlyingType(Type nullableType)
+        {
+            {
+                Type resultType = Nullable.GetUnderlyingType(nullableType);
+                if (resultType != null)
+                    return resultType;
+            }
+
+            if (nullableType.IsGenericType && !nullableType.IsGenericTypeDefinition)
+            {
+                Type genericType = nullableType.GetGenericTypeDefinition();
+                if (genericType == typeof(SerializableNullable<>))
+                    return nullableType.GetGenericArguments()[0];
+            }
+
+            return null;
+        }
+    }
+}
