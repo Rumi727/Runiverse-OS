b33ae78ef144e896e95899db6872d62847b1e82b
Rumi <jun32820@gmail.com>
2025-07-24 20:21:53 +0900

    Update RectOffsetPropertyDrawer.cs

--- Packages/com.rumi.runiengine.core/Editor/Drawers/RectOffsetPropertyDrawer.cs
+++ Packages/com.rumi.runiengine.core/Editor/Drawers/RectOffsetPropertyDrawer.cs
@@ -11,7 +11,18 @@ namespace RuniEngine.Editor.Drawers
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             property.Next(true);
+
+            EditorGUI.BeginProperty(position, label, property);
             EditorGUI.MultiPropertyField(position, labels, property, label);
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (EditorGUIUtility.wideMode)
+                return EditorGUIUtility.singleLineHeight;
+            else
+                return EditorGUIUtility.singleLineHeight * 2;
         }
     }
 }
