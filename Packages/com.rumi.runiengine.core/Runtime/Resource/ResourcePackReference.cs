94c2e5287d3597f0ad13f5214d3450fd3945d260
Rumi <jun32820@gmail.com>
2025-07-26 22:45:20 +0900

    Update ResourcePackReference.cs

--- Packages/com.rumi.runiengine.core/Runtime/Resource/ResourcePackReference.cs
+++ Packages/com.rumi.runiengine.core/Runtime/Resource/ResourcePackReference.cs
@@ -2,10 +2,11 @@
 using Cysharp.Threading.Tasks;
 using Newtonsoft.Json;
 using RuniEngine.IO;
+using System;
 
 namespace RuniEngine.Resource
 {
-    public sealed class ResourcePackReference
+    public sealed class ResourcePackReference : IDisposable
     {
         public static readonly FilePath assetsFolderName = "assets";
         public static readonly FilePath infoPath = "pack.json";
@@ -15,6 +16,8 @@ namespace RuniEngine.Resource
 
         ResourcePackReference()
         {
+            identifier = PackIdentifier.empty;
+            
             rootFolder = IOHandler.empty;
             assetFolder = IOHandler.empty;
             infoFile = IOHandler.empty;
@@ -22,25 +25,27 @@ namespace RuniEngine.Resource
             metaData = new PackMetaData(string.Empty);
         }
 
-        ResourcePackReference(IOHandler folder)
+        ResourcePackReference(PackIdentifier identifier, IOHandler folder)
         {
+            this.identifier = identifier;
+            
             rootFolder = folder;
             assetFolder = folder.CreateChild(assetsFolderName);
             infoFile = folder.CreateChild(infoPath);
         }
 
-        public static async UniTask<ResourcePackReference> GetDefaultPack() => (defaultPack ??= await Create(PackIdentifier.Create("vanilla"), StreamingIOHandler.instance)) ?? emptyPack;
+        public static async UniTask<ResourcePackReference> GetDefaultPack() => (defaultPack ??= await Create(PackIdentifier.CreateByID("vanilla"), StreamingIOHandler.instance)) ?? emptyPack;
 
-        public static UniTask<ResourcePackReference?> Create(FileIOHandler handler) => Create(PackIdentifier.Create(handler.targetPath), handler);
+        public static UniTask<ResourcePackReference?> Create(FileIOHandler handler) => Create(PackIdentifier.CreateByPath(handler.targetPath), handler);
         public static async UniTask<ResourcePackReference?> Create(PackIdentifier packIdentifier, IOHandler handler)
         {
-            ResourcePackReference resourcePack = new ResourcePackReference(handler.Recreate());
+            ResourcePackReference resourcePack = new ResourcePackReference(packIdentifier, handler.Recreate());
             try
             {
                 resourcePack.metaData = JsonConvert.DeserializeObject<PackMetaData>(await resourcePack.infoFile.ReadAllText());
                 resourcePack.isValid = true;
             }
-            catch (System.Exception e)
+            catch (Exception e)
             {
                 Debug.LogException(e);
                 resourcePack.isValid = false;
@@ -49,10 +54,11 @@ namespace RuniEngine.Resource
             if (!resourcePack.isValid)
                 return null;
 
-            _resourcePacks.Add(packIdentifier, resourcePack);
-
+            ResourceManager.internalLoadedResourcePacks.Add(packIdentifier, resourcePack);
             return resourcePack;
         }
+        
+        public PackIdentifier identifier { get; }
 
         public IOHandler rootFolder { get; }
         public IOHandler assetFolder { get; }
@@ -61,5 +67,11 @@ namespace RuniEngine.Resource
         public PackMetaData metaData { get; private set; }
 
         public bool isValid { get; private set; }
+
+        public void Dispose()
+        {
+            isValid = false;
+            ResourceManager.internalLoadedResourcePacks.Remove(identifier);
+        }
     }
 }
