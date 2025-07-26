5ada7518bbd510633e483123ce4f69469e7322f9
Rumi <jun32820@gmail.com>
2025-07-27 01:13:41 +0900

    DotNet 관련 스크립트 위치 이동

--- Packages/com.rumi.runiengine.core/Runtime/Utility/ExceptionUtility.cs
+++ Packages/com.rumi.runiengine.core/Runtime/Utility/ExceptionUtility.cs
@@ -1,10 +1,17 @@
 #nullable enable
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace RuniEngine
 {
     public static class ExceptionUtility
     {
         public static string ToSummaryString(this Exception e) => $"{e.GetType().Name}: {e.Message}\n\n{e.StackTrace.Substring(5)}";
+        
+        public static void ThrowIfArgumentNull([NotNull] object? argument, string? paramName = null)
+        {
+            if (argument is null)
+                throw new ArgumentNullException(paramName ?? nameof(argument));
+        }
     }
 }
