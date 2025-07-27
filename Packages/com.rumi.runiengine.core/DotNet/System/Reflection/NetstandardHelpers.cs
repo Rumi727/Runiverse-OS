c338233124d898f15b39514ada137fd614d2d129
Rumi <jun32820@gmail.com>
2025-07-27 17:02:38 +0900

    NullabilityInfoContext 관련 닷넷 API 추가

---
+++ Packages/com.rumi.runiengine.core/DotNet/System/Reflection/NetstandardHelpers.cs
@@ -0,0 +1,44 @@
+﻿#nullable enable
+// Source : https://source.dot.net/#System.Private.CoreLib/src/libraries/System.Private.CoreLib/src/System/Reflection/NullabilityInfoContext.cs
+using RuniEngine;
+
+#pragma warning disable
+// ReSharper disable all
+namespace System.Reflection
+{
+    public static class NetstandardHelpers
+    {
+        public static MemberInfo GetMemberWithSameMetadataDefinitionAs(this Type type, MemberInfo member)
+        {
+            ExceptionUtility.ThrowIfArgumentNull(member);
+ 
+            const BindingFlags all = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance;
+            foreach (MemberInfo myMemberInfo in type.GetMembers(all))
+            {
+                if (myMemberInfo.HasSameMetadataDefinitionAs(member))
+                {
+                    return myMemberInfo;
+                }
+            }
+ 
+            throw new MissingMemberException(type.FullName, member.Name);
+        }
+
+        static bool HasSameMetadataDefinitionAs(this MemberInfo info, MemberInfo other)
+        {
+            if (info.MetadataToken != other.MetadataToken)
+                return false;
+ 
+            if (!info.Module.Equals(other.Module))
+                return false;
+ 
+            return true;
+        }
+ 
+        public static bool IsGenericMethodParameter(this Type type)
+            => type.IsGenericParameter && type.DeclaringMethod is not null;
+ 
+        public static ReadOnlySpan<ParameterInfo> GetParametersAsSpan(this MethodBase metaMethod)
+            => metaMethod.GetParameters();
+    }
+}
---
+++ Packages/com.rumi.runiengine.core/DotNet/System/Reflection/NullabilityState.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+// Source : https://source.dot.net/#System.Private.CoreLib/src/libraries/System.Private.CoreLib/src/System/Reflection/NullabilityInfo.cs
+
+#pragma warning disable
+// ReSharper disable all
+namespace System.Reflection
+{
+    /// <summary>
+    /// An enum that represents nullability state
+    /// </summary>
+    public enum NullabilityState
+    {
+        /// <summary>
+        /// Nullability context not enabled (oblivious)
+        /// </summary>
+        Unknown,
+        /// <summary>
+        /// Non nullable value or reference type
+        /// </summary>
+        NotNull,
+        /// <summary>
+        /// Nullable value or reference type
+        /// </summary>
+        Nullable
+    }
+}
