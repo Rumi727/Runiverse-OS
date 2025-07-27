2ffef5c59ab43e21bcb491e20019aa65d215669f
Rumi <jun32820@gmail.com>
2025-07-28 07:10:42 +0900

    Add UI Toolkit support to FieldNameAttributeDrawer
    Implements the CreatePropertyGUI method to support Unity's UI Toolkit, allowing FieldNameAttributeDrawer to work with both IMGUI and UI Toolkit property drawers.

--- Packages/com.rumi.runiengine.core/Editor/Drawers/Attributes/FieldNameAttributeDrawer.cs
+++ Packages/com.rumi.runiengine.core/Editor/Drawers/Attributes/FieldNameAttributeDrawer.cs
@@ -1,6 +1,8 @@
 #nullable enable
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 using static RuniEngine.Editor.EditorTool;
 
@@ -9,6 +11,12 @@ namespace RuniEngine.Editor.Drawers.Attributes
     [CustomPropertyDrawer(typeof(FieldNameAttribute))]
     public class FieldNameAttributeDrawer : PropertyDrawer
     {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            FieldNameAttribute attribute = (FieldNameAttribute)this.attribute;
+            return new PropertyField(property, GetTextOrKey(attribute.name));
+        }
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             FieldNameAttribute attribute = (FieldNameAttribute)this.attribute;
