5f2f59c4ca69a8902c3e23ad685a5b399c8f11a7
Rumi <jun32820@gmail.com>
2025-07-27 18:15:36 +0900

    Update AnimFolderPropertyDrawer.cs

--- Packages/com.rumi.runiengine.core/Editor/Drawers/Attributes/AnimFolderPropertyDrawer.cs
+++ Packages/com.rumi.runiengine.core/Editor/Drawers/Attributes/AnimFolderPropertyDrawer.cs
@@ -1,5 +1,6 @@
 #nullable enable
 using System;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.AnimatedValues;
 using UnityEngine;
@@ -11,37 +12,76 @@ namespace RuniEngine.Editor.Drawers.Attributes
     [CustomPropertyDrawer(typeof(AnimFolderAttribute))]
     public class AnimFolderPropertyDrawer : PropertyDrawer
     {
-        AnimBool? animBool;
+        readonly Dictionary<string, AnimFloat> cachedAnimFloat = new();
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             try
             {
-                if (property.IsGeneric() && !property.IsInArray() && !fieldInfo.FieldType.IsAssignableToGenericDefinition(typeof(ISerializableNullable<>)))
+                if (property.IsGeneric() && !property.IsInArray() && !fieldInfo.FieldType.IsAssignableToGenericDefinition(typeof(ISerializableNullable<>))
+                    && cachedAnimFloat.TryGetValue(property.propertyPath, out AnimFloat animFloat) && animFloat.isAnimating)
                 {
                     label = new GUIContent(label); //라벨 복제 안해주면 값 바뀜
-                    animBool ??= new AnimBool(property.isExpanded);
-                
+
+                    float orgHeight;
+                    float headHeight = EditorGUIUtility.singleLineHeight;
+
+                    //높이 계산
                     {
-                        if (animBool.isAnimating)
-                        {
-                            float headHeight = GetYSize(label, EditorStyles.foldout);
-                            float childHeight = EditorGUI.GetPropertyHeight(property, label) - headHeight; //여기에서 값 바뀜
+                        bool isExpanded = property.isExpanded;
+                        property.isExpanded = true;
 
-                            GUI.BeginClip(new Rect(0, 0, position.x + position.width, position.y + headHeight + 3 + 0f.Lerp(childHeight, animBool.faded)));
-                        }
+                        SerializedProperty childProperty = property.Copy();
 
-                        EditorGUI.PropertyField(position, property, label, property.IsGeneric());
-
-                        if (animBool.isAnimating)
-                            GUI.EndClip();
+                        orgHeight = EditorGUI.GetPropertyHeight(childProperty, label); //여기에서 값 바뀜
+                        property.isExpanded = isExpanded;
                     }
 
-                    if (animBool.isAnimating)
-                        RepaintCurrentWindow();
+                    //헤더
+                    {
+                        position.height = headHeight;
+
+                        EditorGUI.BeginProperty(position, label, property);
+                        property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, label, true);
+                        EditorGUI.EndProperty();
+                    }
+
+                    if (property.hasVisibleChildren)
+                    {
+                        float childHeight = orgHeight - headHeight;
+
+                        position.y += headHeight + 2;
+
+                        GUI.BeginClip(new Rect(0, 0, position.x + position.width, position.y + (animFloat.value * childHeight)));
+
+                        if (property.Next(true))
+                        {
+                            int depth = property.depth;
+                            EditorGUI.indentLevel++;
+
+                            do
+                            {
+                                position.height = EditorGUI.GetPropertyHeight(property);
+
+                                BeginLabelWidth(EditorGUIUtility.labelWidth);
+                                EditorGUI.PropertyField(position, property, false);
+                                EndLabelWidth();
+
+                                position.y += position.height + 2;
+                            }
+                            while (property.Next(false) && property.depth == depth);
+
+                            EditorGUI.indentLevel--;
+                        }
+
+                        GUI.EndClip();
+                    }
+
+                    RepaintCurrentWindow();
+                    return;
                 }
-                else
-                    EditorGUI.PropertyField(position, property, label, property.IsGeneric());
+
+                EditorGUI.PropertyField(position, property, label, property.IsGeneric());
             }
             catch (Exception e)
             {
@@ -53,8 +93,12 @@ namespace RuniEngine.Editor.Drawers.Attributes
         {
             if (property.IsGeneric() && !property.IsInArray())
             {
-                animBool ??= new AnimBool(property.isExpanded);
-                animBool.target = property.isExpanded;
+                if (!cachedAnimFloat.TryGetValue(property.propertyPath, out AnimFloat animFloat))
+                {
+                    animFloat = new AnimFloat(property.isExpanded ? 1 : 0);
+                    cachedAnimFloat[property.propertyPath] = animFloat;
+                }
+                animFloat.target = property.isExpanded ? 1 : 0;
 
                 bool isExpanded = property.isExpanded;
 
@@ -63,7 +107,7 @@ namespace RuniEngine.Editor.Drawers.Attributes
                 property.isExpanded = isExpanded;
 
                 float headHeight = GetYSize(label, EditorStyles.foldout) + 3;
-                return headHeight.Lerp(childHeight, animBool.faded);
+                return headHeight.Lerp(childHeight, animFloat.value);
             }
             else
                 return EditorGUI.GetPropertyHeight(property, label, property.IsGeneric());
