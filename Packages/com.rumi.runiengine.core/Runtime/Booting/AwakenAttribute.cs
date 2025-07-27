3e9dd7160ec9a540005594aef62869cbcacaf5a9
Rumi <jun32820@gmail.com>
2025-07-27 22:06:44 +0900

    Update AwakenAttribute.cs

--- Packages/com.rumi.runiengine.core/Runtime/Booting/AwakenAttribute.cs
+++ Packages/com.rumi.runiengine.core/Runtime/Booting/AwakenAttribute.cs
@@ -4,7 +4,8 @@ using System;
 namespace RuniEngine.Booting
 {
     /// <summary>
-    /// 프로젝트 설정이 로드되기 전에 메소드를 호출 시켜주는 어트리뷰트 입니다
+    /// 프로젝트 설정이 로드되기 전에 메소드를 호출 시켜주는 어트리뷰트 입니다<br/>
+    /// <see cref="BootLoader"/> 클래스에서 호출됩니다.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method)]
     public class AwakenAttribute : Attribute { }
