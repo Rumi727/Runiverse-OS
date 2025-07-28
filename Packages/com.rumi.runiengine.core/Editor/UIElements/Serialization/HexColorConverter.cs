96aca0c9066d8e1a70995954ecb14985e3bd575a
Rumi <jun32820@gmail.com>
2025-07-29 08:56:29 +0900

    Create HexColorConverter.cs

---
+++ Packages/com.rumi.runiengine.core/Editor/UIElements/Serialization/HexColorConverter.cs
@@ -0,0 +1,11 @@
+#nullable enable
+using UnityEditor.UIElements;
+
+namespace RuniEngine.Editor.UIElements.Serialization
+{
+    public sealed class HexColorConverter : UxmlAttributeConverter<HexColor>
+    {
+        public override HexColor FromString(string value) => new HexColor(value);
+        public override string ToString(HexColor value) => value.ToString();
+    }
+}
