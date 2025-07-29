cb31bb7deac48e2f1cbf636573d78938f9c5c250
Rumi <jun32820@gmail.com>
2025-07-29 10:08:00 +0900

    Create ScriptAttributeUtility Bridge

---
+++ Packages/com.rumi.runiengine.core/APIBridge/Editor/UnityEditor/ScriptAttributeUtility.cs
@@ -0,0 +1,30 @@
+﻿#nullable enable
+using System;
+using System.Reflection;
+using UnityEditor;
+
+namespace RuniEngine.Editor.APIBridge.UnityEditor
+{
+    public class ScriptAttributeUtility
+    {
+        public static Type type { get; } = EditorAssemblyManager.UnityEditor_CoreModule.GetType("UnityEditor.ScriptAttributeUtility");
+        
+        protected ScriptAttributeUtility() { }
+        
+        static MethodInfo? m_GetFieldInfoFromProperty;
+        static readonly object?[] mp_GetFieldInfoFromProperty = new object?[2];
+        static readonly Type[] mpt_GetFieldInfoFromProperty = new Type[] { typeof(SerializedProperty), typeof(Type) };
+        public FieldInfo? GetFieldInfoFromProperty(SerializedProperty property, out Type? type)
+        {
+            m_GetFieldInfoFromProperty ??= ScriptAttributeUtility.type.GetMethod("GetFieldInfoFromProperty", BindingFlags.NonPublic | BindingFlags.Static, null, mpt_GetFieldInfoFromProperty, null);
+
+            mp_GetFieldInfoFromProperty[0] = property;
+            mp_GetFieldInfoFromProperty[1] = null;
+            
+            FieldInfo? result = (FieldInfo?)m_GetFieldInfoFromProperty!.Invoke(null, mp_GetFieldInfoFromProperty);
+            
+            type = (Type?)mp_GetFieldInfoFromProperty[1];
+            return result;
+        }
+    }
+}
