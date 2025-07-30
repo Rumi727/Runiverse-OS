15ed27766cff578d7450a2f157e7d0698790eef7
Rumi <jun32820@gmail.com>
2025-07-31 04:28:32 +0900

    SerializableNullable.GetUnderlyingType 수정

--- Packages/com.rumi.runios.core/Runtime/Structs/SerializableNullable.cs
+++ Packages/com.rumi.runios.core/Runtime/Structs/SerializableNullable.cs
@@ -18,28 +18,28 @@ namespace RuniOS
         public const string nameofHasValue = "hasValue";
         
         /// <summary>
-        /// 지정된 nullable 타입의 기본 (<see cref="Nullable{T}"/> 또는 <see cref="ISerializableNullable{T}"/>의 내부) 타입을 가져옵니다.
+        /// 지정된 nullable 타입의 기본 (<see cref="ISerializableNullable{T}"/>의 내부) 타입을 가져옵니다.
         /// </summary>
-        /// <param name="nullableType">기본 타입을 가져올 nullable 타입입니다 (예: <c>int?</c> 또는 <c>SerializableNullable&lt;int&gt;</c>).</param>
+        /// <param name="nullableType">기본 타입을 가져올 nullable 타입입니다 (예: <c>SerializableNullable&lt;int&gt;</c>).</param>
         /// <returns>
-        /// <paramref name="nullableType"/>이 <see cref="Nullable{T}"/> 또는 <see cref="ISerializableNullable{T}"/>의 인스턴스이면 해당 T 타입이고,
+        /// <paramref name="nullableType"/>이 <see cref="ISerializableNullable{T}"/>의 인스턴스이면 해당 T 타입이고,
         /// 그렇지 않으면 <see langword="null"/>입니다.
         /// </returns>
         public static Type? GetUnderlyingType(Type nullableType)
         {
-            {
-                // 표준 Nullable<T>의 기본 타입을 확인합니다.
-                Type? resultType = Nullable.GetUnderlyingType(nullableType);
-                if (resultType != null)
-                    return resultType;
-            }
-
             // 제네릭 타입이고 제네릭 정의가 아닌 경우에만 처리합니다.
             if (nullableType.IsGenericType && !nullableType.IsGenericTypeDefinition)
             {
                 // ISerializableNullable<>의 제네릭 정의에 할당 가능한지 확인합니다.
                 if (nullableType.IsAssignableToGenericDefinition(typeof(ISerializableNullable<>)))
-                    return nullableType.GetGenericArguments()[0]; // 제네릭 인자(T)를 반환합니다.
+                {
+                    Type type = nullableType.GetGenericArguments()[0];
+                    Type? underlyingType = GetUnderlyingType(type);
+                    if (underlyingType != null)
+                        return underlyingType; // 제네릭 인자(T)를 반환합니다.
+                    else
+                        return type;
+                }
             }
 
             return null;
