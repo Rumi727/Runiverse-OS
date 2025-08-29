eff24a60644a60dbb14e4059d94afee017940171
Rumi <jun32820@gmail.com>
2025-08-30 00:52:18 +0900

    Add GetSequenceHashCode<T>(this IEnumerable<T>?)

--- Packages/com.rumi.runios.core/Runtime/Utility/LinqExtras.cs
+++ Packages/com.rumi.runios.core/Runtime/Utility/LinqExtras.cs
@@ -34,5 +34,24 @@ namespace System.Linq
         }
 
         public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source) => source.Where(static x => x != null)!;
+        
+        /// <summary>
+        /// 지정된 열거형의 순서와 내용을 기반으로 해시코드를 생성합니다.<br/>
+        /// 열거형의 요소가 순서까지 같으면 동일한 해시코드를 반환합니다.
+        /// </summary>
+        /// <param name="list">해시코드를 계산할 리스트 또는 배열입니다.</param>
+        /// <typeparam name="T">리스트 또는 배열의 요소 타입입니다.</typeparam>
+        /// <returns>생성된 해시코드를 반환합니다.</returns>
+        public static int GetSequenceHashCode<T>(this IEnumerable<T>? list)
+        {
+            if (list == null)
+                return 0;
+            
+            var hash = new HashCode();
+            foreach (var item in list)
+                hash.Add(item);
+
+            return hash.ToHashCode();
+        }
     }
 }
