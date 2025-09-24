31819ce4ce663d61f07d149215edff8d960ef5c1
Rumi <jun32820@gmail.com>
2025-09-24 16:43:57 +0900

    ReadOnlyQueue, ReadOnlyStack 추가

---
+++ Packages/com.rumi.runios.core/Runtime/Collections/Generic/ReadOnlyQueue.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RuniOS.Collections.Generic
+{
+    public sealed class ReadOnlyQueue<T> : IReadOnlyCollection<T>
+    {
+        public ReadOnlyQueue(Queue<T> queue) => this.queue = queue;
+
+        readonly Queue<T> queue;
+
+        public int Count => queue.Count;
+
+        public bool Contains(T item) => queue.Contains(item);
+        public void CopyTo(T[] array, int arrayIndex) => queue.CopyTo(array, arrayIndex);
+
+        public T Peek() => queue.Peek();
+        public T[] ToArray() => queue.ToArray();
+
+        public bool TryPeek(out T result) => queue.TryPeek(out result);
+        
+        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)queue).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)queue).GetEnumerator();
+    }
+}
---
+++ Packages/com.rumi.runios.core/Runtime/Collections/Generic/ReadOnlyStack.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RuniOS.Collections.Generic
+{
+    public sealed class ReadOnlyStack<T> : IReadOnlyCollection<T>
+    {
+        public ReadOnlyStack(Stack<T> stack) => this.stack = stack;
+
+        readonly Stack<T> stack;
+
+        public int Count => stack.Count;
+
+        public bool Contains(T item) => stack.Contains(item);
+        public void CopyTo(T[] array, int arrayIndex) => stack.CopyTo(array, arrayIndex);
+
+        public T Peek() => stack.Peek();
+        public T[] ToArray() => stack.ToArray();
+
+        public bool TryPeek(out T result) => stack.TryPeek(out result);
+        
+        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)stack).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)stack).GetEnumerator();
+    }
+}
