717fb0141d2283a43dba17cdc13d76d9b798b8a7
Rumi <jun32820@gmail.com>
2025-09-24 16:40:03 +0900

    라벨 필드에 uss 클래스 추가

--- Packages/com.rumi.runios.core/Runtime/UIElements/LabelField.cs
+++ Packages/com.rumi.runios.core/Runtime/UIElements/LabelField.cs
@@ -5,17 +5,23 @@ namespace RuniOS.UIElements
     [UxmlElement]
     public partial class LabelField : BaseField<string>
     {
+        public new const string ussClassName = "runios-label-field";
+        public new const string labelUssClassName = ussClassName + "__label";
+        public new const string inputUssClassName = ussClassName + "__input";
+        
         public Label visualInput { get; }
         
-        public LabelField() : this(null, string.Empty) { }
-        public LabelField(string? label, string text) : base(label, new Label())
+        public LabelField() : this(string.Empty, string.Empty) { }
+        public LabelField(string label) : this(label, string.Empty) { }
+        public LabelField(string label, string text) : base(label, new Label())
         {
             this.RegisterDefaultStyleSheet(UIToolkitUtility.rosControlStyle);
             
-            labelElement.AddToClassList(labelUssClassName);
             AddToClassList(ussClassName);
+            labelElement.AddToClassList(labelUssClassName);
             
             visualInput = this.Q<Label>(className: BaseField<SerializableType>.inputUssClassName);
+            visualInput.AddToClassList(inputUssClassName);
             visualInput.SetValueWithoutNotify(text);
         }
 
