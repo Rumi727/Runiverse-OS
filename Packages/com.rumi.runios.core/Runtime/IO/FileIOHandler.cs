e4e15c8284cebd8006771c06a51028b044810738
Rumi <jun32820@gmail.com>
2025-10-06 08:16:48 +0900

    Update FileIOHandler.cs

--- Packages/com.rumi.runios.core/Runtime/IO/FileIOHandler.cs
+++ Packages/com.rumi.runios.core/Runtime/IO/FileIOHandler.cs
@@ -82,13 +82,13 @@ namespace RuniOS.IO
         /// 이 핸들러가 나타내는 디렉토리가 존재하는지 비동기적으로 확인합니다.
         /// </summary>
         /// <returns>디렉토리가 존재하면 <see langword="true"/>, 그렇지 않으면 <see langword="false"/>를 반환하는 <see cref="bool"/>입니다.</returns>
-        public override UniTask<bool> DirectoryExists() => UniTask.RunOnThreadPool(() => Directory.Exists(targetPath));
+        public override UniTask<bool> DirectoryExists() => UniTask.FromResult(Directory.Exists(targetPath));
 
         /// <summary>
         /// 이 핸들러가 나타내는 파일이 존재하는지 비동기적으로 확인합니다.
         /// </summary>
         /// <returns>파일이 존재하면 <see langword="true"/>, 그렇지 않으면 <see langword="false"/>를 반환하는 <see cref="bool"/>입니다.</returns>
-        public override UniTask<bool> FileExists() => UniTask.RunOnThreadPool(() => File.Exists(targetPath));
+        public override UniTask<bool> FileExists() => UniTask.FromResult(File.Exists(targetPath));
 
         /// <summary>
         /// 이 핸들러가 나타내는 디렉토리의 모든 서브디렉토리 이름을 비동기적으로 열거합니다.
@@ -97,7 +97,7 @@ namespace RuniOS.IO
         /// <exception cref="UnauthorizedAccessException">호출자에게 필요한 권한이 없는 경우 발생합니다.</exception>
         /// <exception cref="DirectoryNotFoundException">지정된 경로의 일부가 유효하지 않은 경우 발생합니다.</exception>
         /// <exception cref="IOException">I/O 오류가 발생한 경우 발생합니다.</exception>
-        public override UniTask<IEnumerable<string>> GetDirectories() => UniTask.RunOnThreadPool(() => Directory.EnumerateDirectories(targetPath).Select(x =>
+        public override UniTask<IEnumerable<string>> GetDirectories() => UniTask.FromResult(Directory.EnumerateDirectories(targetPath).Select(x =>
         {
             if (x.ToPath().TryTrimStartPath(targetPath, out FilePath result))
                 return result.ToString();
@@ -112,7 +112,7 @@ namespace RuniOS.IO
         /// <exception cref="UnauthorizedAccessException">호출자에게 필요한 권한이 없는 경우 발생합니다.</exception>
         /// <exception cref="DirectoryNotFoundException">지정된 경로의 일부가 유효하지 않은 경우 발생합니다.</exception>
         /// <exception cref="IOException">I/O 오류가 발생한 경우 발생합니다.</exception>
-        public override UniTask<IEnumerable<FilePath>> GetAllDirectories() => UniTask.RunOnThreadPool(() => Directory.EnumerateDirectories(targetPath, "*", SearchOption.AllDirectories).Select(x => x - targetPath));
+        public override UniTask<IEnumerable<FilePath>> GetAllDirectories() => UniTask.FromResult(Directory.EnumerateDirectories(targetPath, "*", SearchOption.AllDirectories).Select(x => x - targetPath));
 
         /// <summary>
         /// 이 핸들러가 나타내는 디렉토리의 모든 파일 이름을 비동기적으로 열거합니다.
@@ -121,7 +121,7 @@ namespace RuniOS.IO
         /// <exception cref="UnauthorizedAccessException">호출자에게 필요한 권한이 없는 경우 발생합니다.</exception>
         /// <exception cref="DirectoryNotFoundException">지정된 경로의 일부가 유효하지 않은 경우 발생합니다.</exception>
         /// <exception cref="IOException">I/O 오류가 발생한 경우 발생합니다.</exception>
-        public override UniTask<IEnumerable<string>> GetFiles() => UniTask.RunOnThreadPool(() => Directory.EnumerateFiles(targetPath).Select(x =>
+        public override UniTask<IEnumerable<string>> GetFiles() => UniTask.FromResult(Directory.EnumerateFiles(targetPath).Select(x =>
         {
             if (x.ToPath().TryTrimStartPath(targetPath, out FilePath result))
                 return result.ToString();
@@ -137,7 +137,7 @@ namespace RuniOS.IO
         /// <exception cref="UnauthorizedAccessException">호출자에게 필요한 권한이 없는 경우 발생합니다.</exception>
         /// <exception cref="DirectoryNotFoundException">지정된 경로의 일부가 유효하지 않은 경우 발생합니다.</exception>
         /// <exception cref="IOException">I/O 오류가 발생한 경우 발생합니다.</exception>
-        public override UniTask<IEnumerable<string>> GetFiles(WildcardPatterns wildcardPatterns) => UniTask.RunOnThreadPool(() => DirectoryUtility.EnumerateFiles(targetPath, wildcardPatterns).Select(x =>
+        public override UniTask<IEnumerable<string>> GetFiles(WildcardPatterns wildcardPatterns) => UniTask.FromResult(DirectoryUtility.EnumerateFiles(targetPath, wildcardPatterns).Select(x =>
         {
             FilePath path = x - targetPath;
             if (path != targetPath)
@@ -153,7 +153,7 @@ namespace RuniOS.IO
         /// <exception cref="UnauthorizedAccessException">호출자에게 필요한 권한이 없는 경우 발생합니다.</exception>
         /// <exception cref="DirectoryNotFoundException">지정된 경로의 일부가 유효하지 않은 경우 발생합니다.</exception>
         /// <exception cref="IOException">I/O 오류가 발생한 경우 발생합니다.</exception>
-        public override UniTask<IEnumerable<FilePath>> GetAllFiles() => UniTask.RunOnThreadPool(() => Directory.EnumerateFiles(targetPath, "*", SearchOption.AllDirectories).Select(x => x - targetPath));
+        public override UniTask<IEnumerable<FilePath>> GetAllFiles() => UniTask.FromResult(Directory.EnumerateFiles(targetPath, "*", SearchOption.AllDirectories).Select(x => x - targetPath));
 
         /// <summary>
         /// 지정된 와일드카드 패턴과 일치하는, 이 핸들러가 나타내는 디렉토리의 모든 파일 경로(재귀적으로)를 비동기적으로 열거합니다.
@@ -163,7 +163,7 @@ namespace RuniOS.IO
         /// <exception cref="UnauthorizedAccessException">호출자에게 필요한 권한이 없는 경우 발생합니다.</exception>
         /// <exception cref="DirectoryNotFoundException">지정된 경로의 일부가 유효하지 않은 경우 발생합니다.</exception>
         /// <exception cref="IOException">I/O 오류가 발생한 경우 발생합니다.</exception>
-        public override UniTask<IEnumerable<FilePath>> GetAllFiles(WildcardPatterns wildcardPatterns) => UniTask.RunOnThreadPool(() => DirectoryUtility.EnumerateFiles(targetPath, wildcardPatterns, SearchOption.AllDirectories).Select(x => x - targetPath));
+        public override UniTask<IEnumerable<FilePath>> GetAllFiles(WildcardPatterns wildcardPatterns) => UniTask.FromResult(DirectoryUtility.EnumerateFiles(targetPath, wildcardPatterns, SearchOption.AllDirectories).Select(x => x - targetPath));
 
         /// <summary>
         /// 이 핸들러가 나타내는 파일의 모든 바이트를 비동기적으로 읽습니다.
@@ -202,7 +202,7 @@ namespace RuniOS.IO
         /// <exception cref="UnauthorizedAccessException">호출자에게 필요한 권한이 없는 경우 발생합니다.</exception>
         /// <exception cref="PathTooLongException">경로가 시스템 정의 최대 길이를 초과하는 경우 발생합니다.</exception>
         /// <exception cref="NotSupportedException">경로에 콜론(:)이 포함된 경우 발생합니다.</exception>
-        public override UniTask<IEnumerable<string>> ReadLines() => UniTask.RunOnThreadPool(() => File.ReadLines(targetPath));
+        public override UniTask<IEnumerable<string>> ReadLines() => UniTask.FromResult(File.ReadLines(targetPath));
 
         /// <summary>
         /// 이 핸들러가 나타내는 파일을 읽기 모드로 열어 스트림을 비동기적으로 반환합니다.
@@ -215,6 +215,6 @@ namespace RuniOS.IO
         /// <exception cref="UnauthorizedAccessException">호출자에게 필요한 권한이 없는 경우 발생합니다.</exception>
         /// <exception cref="PathTooLongException">경로가 시스템 정의 최대 길이를 초과하는 경우 발생합니다.</exception>
         /// <exception cref="NotSupportedException">경로에 콜론(:)이 포함된 경우 발생합니다.</exception>
-        public override UniTask<Stream> OpenRead() => UniTask.RunOnThreadPool(() => (Stream)File.OpenRead(targetPath));
+        public override UniTask<Stream> OpenRead() => UniTask.FromResult<Stream>(File.OpenRead(targetPath));
     }
 }
