a540f007cf6b9a636744632745100c99b74a45db
Rumi <jun32820@gmail.com>
2025-10-06 22:51:29 +0900

    Update Kernel.Time.cs

--- Packages/com.rumi.runios.core/Runtime/Kernel.Time.cs
+++ Packages/com.rumi.runios.core/Runtime/Kernel.Time.cs
@@ -27,26 +27,17 @@ namespace RuniOS
 
         public const float fps60Second = 1f / 60f;
 
-        /// <summary>
-        /// 게임의 전체 속도를 결정 합니다
-        /// </summary>
-        public static float gameSpeed
-        {
-            get => Time.timeScale;
-            set => Time.timeScale = value;
-        }
-
         static void TimeUpdate()
         {
             double realDeltaTime = deltaTimeStopwatch.Elapsed.TotalSeconds;
             deltaTimeStopwatch.Restart();
 
-            float gameSpeed = Kernel.gameSpeed;
+            float timeScale = Time.timeScale;
 
             //유니티의 내장 변수들은 테스트 결과, 약간의 성능을 더 먹는것으로 확인되었기 때문에
             //이렇게 관리 스크립트가 변수를 할당하고 다른 스크립트가 그 변수를 가져오는것이 성능에 더 도움 되는것을 확인하였습니다
-            deltaTime = (float)realDeltaTime * gameSpeed;
-            deltaTimeDouble = realDeltaTime * gameSpeed;
+            deltaTime = (float)realDeltaTime * timeScale;
+            deltaTimeDouble = realDeltaTime * timeScale;
             unscaledDeltaTime = (float)realDeltaTime;
             unscaledDeltaTimeDouble = realDeltaTime;
 
@@ -56,7 +47,7 @@ namespace RuniOS
             //테스트 결과 유니티 내부 구현이랑 100% 일치하진 않지만 오차를 무시할 수 있을 만큼 일치합니다
             {
                 unscaledSmoothDeltaTime = (0.2f * unscaledDeltaTime) + ((1 - 0.2f) * unscaledSmoothDeltaTime);
-                smoothDeltaTime = unscaledDeltaTime * gameSpeed;
+                smoothDeltaTime = unscaledDeltaTime * timeScale;
             }
         }
     }
