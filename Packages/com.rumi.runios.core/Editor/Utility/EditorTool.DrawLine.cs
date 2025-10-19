6f4c0afa3eea2b7258557d61729f4f416fe78eb4
Rumi <jun32820@gmail.com>
2025-10-20 02:12:13 +0900

    EditorTool.DrawHLine 기본 색 수정

--- Packages/com.rumi.runios.core/Editor/Utility/EditorTool.DrawLine.cs
+++ Packages/com.rumi.runios.core/Editor/Utility/EditorTool.DrawLine.cs
@@ -6,15 +6,15 @@ namespace RuniOS.Editor
 {
     public partial class EditorTool
     {
-        public static void DrawHLine(int thickness = 1, int padding = 10) => DrawHLine(new Color(0.4980392f, 0.4980392f, 0.4980392f), thickness, padding);
+        public static void DrawHLine(int thickness = 1, int padding = 10) => DrawHLine(EditorGUIUtility.isProSkin ? new Color32(26, 26, 26, 255) : new Color32(127, 127, 127, 255), thickness, padding);
 
         public static void DrawHLine(Color color, int thickness = 1, int padding = 10)
         {
             Rect r = EditorGUILayout.GetControlRect(GUILayout.Height(padding - 2));
             r.height = thickness;
             r.y += (padding / 2f) - 2;
-            r.x -= 18;
-            r.width += 22;
+            r.x -= 3;
+            r.width += 6;
             EditorGUI.DrawRect(r, color);
         }
     }
