d537ca85074520ddc86a905501f89922ac6a38b6
Rumi <jun32820@gmail.com>
2025-12-08 21:27:58 +0900

    CS0695 해결

--- Packages/com.rumi.runios.core/Runtime/Resource/IAssetRegistry`1.cs
+++ Packages/com.rumi.runios.core/Runtime/Resource/IAssetRegistry`1.cs
@@ -1,39 +1,35 @@
 #nullable enable
+using System.Diagnostics.CodeAnalysis;
+
 namespace RuniOS.Resource
 {
-    public interface IAssetRegistry<THandle> : IAssetRegistry, IReadOnlyDictionary<Identifier, THandle> where THandle : class, IAssetHandle
+    public interface IAssetRegistry<THandle> : IAssetRegistry where THandle : class, IAssetHandle
     {
-        new THandle this[Identifier key] { get; }
-        THandle IReadOnlyDictionary<Identifier, THandle>.this[Identifier key] => this[key];
-        IAssetHandle IReadOnlyDictionary<Identifier, IAssetHandle>.this[Identifier key] => this[key];
-        
         Type IAssetRegistry.handleType => typeof(THandle);
-
-        new int Count { get; }
-        int IReadOnlyCollection<KeyValuePair<Identifier, THandle>>.Count => Count;
-        int IReadOnlyCollection<KeyValuePair<Identifier, IAssetHandle>>.Count => Count;
+        
+        new THandle this[Identifier key] { get; }
+        IAssetHandle IReadOnlyDictionary<Identifier, IAssetHandle>.this[Identifier key] => this[key];
 
         new IEnumerable<Identifier> Keys { get; }
-        IEnumerable<Identifier> IReadOnlyDictionary<Identifier, THandle>.Keys => Keys;
         IEnumerable<Identifier> IReadOnlyDictionary<Identifier, IAssetHandle>.Keys => Keys;
         
         new IEnumerable<THandle> Values { get; }
-        IEnumerable<THandle> IReadOnlyDictionary<Identifier, THandle>.Values => Values;
         IEnumerable<IAssetHandle> IReadOnlyDictionary<Identifier, IAssetHandle>.Values => Values;
 
-        new bool ContainsKey(Identifier key);
-        bool IReadOnlyDictionary<Identifier, THandle>.ContainsKey(Identifier key) => ContainsKey(key);
-        bool IReadOnlyDictionary<Identifier, IAssetHandle>.ContainsKey(Identifier key) => ContainsKey(key);
-
+        bool TryGetValue(Identifier key, [NotNullWhen(true)] out THandle? value);
         bool IReadOnlyDictionary<Identifier, IAssetHandle>.TryGetValue(Identifier key, out IAssetHandle value)
         {
-            bool result = TryGetValue(key, out THandle? genericValue);
-            value = genericValue!;
-            return result;
+            if (TryGetValue(key, out THandle? genericValue))
+            {
+                value = genericValue;
+                return true;
+            }
+            
+            value = null!;
+            return false;
         }
 
         new IEnumerator<KeyValuePair<Identifier, THandle>> GetEnumerator();
-        IEnumerator<KeyValuePair<Identifier, THandle>> IEnumerable<KeyValuePair<Identifier, THandle>>.GetEnumerator() => GetEnumerator();
         IEnumerator<KeyValuePair<Identifier, IAssetHandle>> IEnumerable<KeyValuePair<Identifier, IAssetHandle>>.GetEnumerator()
         {
             foreach (var item in this)
