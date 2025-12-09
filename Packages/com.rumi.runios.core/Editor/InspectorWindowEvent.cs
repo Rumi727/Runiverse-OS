4414cbe18cf1764d1ad7d0be7471d58eafa15341
Rumi <jun32820@gmail.com>
2025-12-09 23:08:19 +0900

    인스펙터 커스텀을 위한 InspectorWindowEvent.onGUI 추가

---
+++ Packages/com.rumi.runios.core/Editor/InspectorWindowEvent.cs
@@ -0,0 +1,10 @@
+﻿using RuniOS.Editor.APIBridge.UnityEditor;
+
+namespace RuniOS.Editor
+{
+    public static class InspectorWindowEvent
+    {
+        public static event Action<InspectorWindowBridge>? onGUI { add => _onGUI += value; remove => _onGUI -= value; }
+        internal static Action<InspectorWindowBridge>? _onGUI; // Patches/Editor/UnityEditor.PropertyEditor.cs를 참고해주세요
+    }
+}
---
+++ Packages/com.rumi.runios.core/Patches/Editor/UnityEditor.PropertyEditor.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+using HarmonyLib;
+using RuniOS.Editor.APIBridge.UnityEditor;
+using System.Reflection;
+using UnityEngine.UIElements;
+
+namespace RuniOS.Editor.Patches
+{
+    public static partial class Patches
+    {
+        public static partial class UnityEditorPatch
+        {
+            public static class PropertyEditorPatch
+            {
+                [HarmonyPatch]
+                public static class RebuildContentsContainers
+                {
+                    public static MethodBase TargetMethod() => AccessTools.DeclaredMethod(PropertyEditorBridge.__targetType, nameof(PropertyEditorBridge.RebuildContentsContainers));
+
+                    public static void Postfix(EditorWindow __instance)
+                    {
+                        if (!InspectorWindowBridge.__targetType.IsInstanceOfType(__instance))
+                            return;
+                        
+                        PropertyEditorBridge propertyEditor = PropertyEditorBridge.__GetInstanceFrom(__instance);
+                        InspectorWindowBridge inspectorWindow = InspectorWindowBridge.__GetInstanceFrom(__instance);
+                        
+                        IMGUIContainer imguiContainer = propertyEditor.CreateIMGUIContainer(() => InspectorWindowEvent._onGUI?.SafeInvoke(inspectorWindow), typeof(InspectorWindowEvent).FullName);
+                        propertyEditor.editorsElement.Add(imguiContainer);
+                    }
+                }
+            }
+        }
+    }
+}
