f64cd11f1c95ccf10386aeee18b44b50395f1210
Rumi <jun32820@gmail.com>
2025-12-12 07:48:55 +0900

    Update ScriptingDefineSymbolSetter.cs

--- Packages/com.rumi.runios.core/Editor/ScriptingDefineSymbolSetter.cs
+++ Packages/com.rumi.runios.core/Editor/ScriptingDefineSymbolSetter.cs
@@ -10,28 +10,43 @@ namespace RuniOS.Editor
         {
             const string symbolName = "RUNI_ENGINE";
 
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.Android, symbolName);
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.EmbeddedLinux, symbolName);
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.iOS, symbolName);
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.LinuxHeadlessSimulation, symbolName);
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.NintendoSwitch, symbolName);
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.PS4, symbolName);
-#if UNITY_2023_1_OR_NEWER
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.PS5, symbolName);
+            AddSymbol(NamedBuildTarget.Android, symbolName);
+            AddSymbol(NamedBuildTarget.EmbeddedLinux, symbolName);
+            AddSymbol(NamedBuildTarget.iOS, symbolName);
+            AddSymbol(NamedBuildTarget.LinuxHeadlessSimulation, symbolName);
+            AddSymbol(NamedBuildTarget.NintendoSwitch, symbolName);
+            AddSymbol(NamedBuildTarget.PS4, symbolName);
+            AddSymbol(NamedBuildTarget.PS5, symbolName);
+            AddSymbol(NamedBuildTarget.Server, symbolName);
+            AddSymbol(NamedBuildTarget.Standalone, symbolName);
+            AddSymbol(NamedBuildTarget.tvOS, symbolName);
+            AddSymbol(NamedBuildTarget.WebGL, symbolName);
+            AddSymbol(NamedBuildTarget.WindowsStoreApps, symbolName);
+            AddSymbol(NamedBuildTarget.XboxOne, symbolName);
+            AddSymbol(NamedBuildTarget.FromBuildTargetGroup(BuildTargetGroup.GameCoreXboxSeries), symbolName); // Xbox Series X|S
+            AddSymbol(NamedBuildTarget.FromBuildTargetGroup(BuildTargetGroup.GameCoreXboxOne), symbolName);  // Xbox One (GDK)
+            AddSymbol(NamedBuildTarget.FromBuildTargetGroup(BuildTargetGroup.Kepler), symbolName);
+#if UNITY_2022_1_OR_NEWER
+            AddSymbol(NamedBuildTarget.QNX, symbolName);
 #endif
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.QNX, symbolName);
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.Server, symbolName);
 #if !UNITY_2023_1_OR_NEWER
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.Stadia, symbolName);
+            AddSymbol(NamedBuildTarget.Stadia, symbolName);
 #endif
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.Standalone, symbolName);
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.tvOS, symbolName);
-#if UNITY_6000_1_ORNEWER
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.VisionOS, symbolName);
+#if UNITY_6000_1_OR_NEWER
+            AddSymbol(NamedBuildTarget.VisionOS, symbolName);
+            AddSymbol(NamedBuildTarget.NintendoSwitch2, symbolName);
 #endif
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.WebGL, symbolName);
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.WindowsStoreApps, symbolName);
-            PlayerSettings.SetScriptingDefineSymbols(NamedBuildTarget.XboxOne, symbolName);
+        }
+
+        static void AddSymbol(NamedBuildTarget target, string symbol)
+        {
+            string currentSymbols = PlayerSettings.GetScriptingDefineSymbols(target);
+            var defines = currentSymbols.Split(';', StringSplitOptions.RemoveEmptyEntries);
+            if (!defines.Contains(symbol))
+            {
+                defines = defines.Add(symbol);
+                PlayerSettings.SetScriptingDefineSymbols(target, string.Join(";", defines));
+            }
         }
     }
 }
