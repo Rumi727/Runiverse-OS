41cb6c84c83b85dac016a675930d1443053d9829
Rumi <jun32820@gmail.com>
2025-12-11 23:57:53 +0900

    EditorTool.PathIsProjectPath -> EditorTool.IsProjectPath

--- Packages/com.rumi.runios.core/Editor/Utility/EditorTool.Path.cs
+++ Packages/com.rumi.runios.core/Editor/Utility/EditorTool.Path.cs
@@ -8,17 +8,6 @@ namespace RuniOS.Editor
     {
         public static FilePath projectPath { get; } = Directory.GetCurrentDirectory();
 
-        public static bool PathIsProjectPath(string path)
-        {
-            path = path.Replace("\\", "/");
-            string projectPath = Directory.GetCurrentDirectory();
-
-            if (path.StartsWith(Path.Combine(projectPath, "Assets").Replace("\\", "/"), StringComparison.Ordinal))
-                return true;
-            else if (path.StartsWith(Path.Combine(projectPath, "Packages").Replace("\\", "/"), StringComparison.Ordinal))
-                return true;
-
-            return false;
-        }
+        public static bool IsProjectPath(FilePath path) => path.StartsWith(projectPath + "Assets") || path.StartsWith(projectPath + "Packages");
     }
 }
