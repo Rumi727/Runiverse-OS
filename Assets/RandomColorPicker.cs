a4524be79d0fd9c68fd2c39bb609796ea3022497
Rumi <jun32820@gmail.com>
2026-03-06 22:03:22 +0900

    Create RandomColorPicker.cs

---
+++ Assets/RandomColorPicker.cs
@@ -0,0 +1,17 @@
+﻿using RuniOS;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomColorPicker : MonoBehaviour
+{
+    public Image image;
+    public TMP_Text text;
+    
+    public void ColorUpdate()
+    {
+        Color color = new Color32((byte)Random.Range(0, 255), (byte)Random.Range(0, 255), (byte)Random.Range(0, 255), 255);
+        image.color = color;
+        text.text = ((HexColor)color).ToString();
+    }
+}
