b27105b051445ad1290d43f1d7be02e5f9c480cc
Rumi <jun32820@gmail.com>
2026-05-12 11:54:18 +0900

    Update VirtualFileBase.cs

--- Packages/com.rumi.runios.core/Runtime/IO/Virtual/VirtualFileBase.cs
+++ Packages/com.rumi.runios.core/Runtime/IO/Virtual/VirtualFileBase.cs
@@ -1,12 +1,13 @@
 ﻿#nullable enable
 using Cysharp.Threading.Tasks;
 using System.IO;
+using System.Threading;
 
 namespace RuniOS.IO.Virtual
 {
     public abstract class VirtualFileBase : VirtualNode
     {
-        public abstract UniTask<Stream> OpenRead();
-        public abstract UniTask<Stream> OpenWrite();
+        public abstract UniTask<Stream> OpenRead(CancellationToken cancellationToken = default);
+        public abstract UniTask<Stream> OpenWrite(CancellationToken cancellationToken = default);
     }
 }
