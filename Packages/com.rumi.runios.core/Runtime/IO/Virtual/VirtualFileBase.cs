c60dcb35199b6241c8fd87e72d509cff584ec6e1
Rumi <jun32820@gmail.com>
2026-05-12 11:56:08 +0900

    Update VirtualFileBase.cs

--- Packages/com.rumi.runios.core/Runtime/IO/Virtual/VirtualFileBase.cs
+++ Packages/com.rumi.runios.core/Runtime/IO/Virtual/VirtualFileBase.cs
@@ -7,6 +7,8 @@ namespace RuniOS.IO.Virtual
 {
     public abstract class VirtualFileBase : VirtualNode
     {
+        // TODO : ReadAll*, WriteAll* 계열 추가
+        
         public abstract UniTask<Stream> OpenRead(CancellationToken cancellationToken = default);
         public abstract UniTask<Stream> OpenWrite(CancellationToken cancellationToken = default);
     }
