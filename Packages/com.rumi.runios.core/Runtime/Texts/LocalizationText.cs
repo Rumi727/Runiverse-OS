1ef07687b8f3e62f6966453b3232224d19c50f0c
Rumi <jun32820@gmail.com>
2026-06-18 21:04:14 +0900

    readonly -> writeable

--- Packages/com.rumi.runios.core/Runtime/Texts/LocalizationText.cs
+++ Packages/com.rumi.runios.core/Runtime/Texts/LocalizationText.cs
@@ -19,19 +19,19 @@ namespace RuniOS.Texts
     /// The language code to use, or <see langword="null"/> to use the current default language.<br/>
     /// 사용할 언어 코드이며, <see langword="null"/>이면 현재 기본 언어를 사용합니다.
     /// </param>
-    public class LocalizationText(Identifier identifier, IReadOnlyList<Text> args, string? languageCode = null) : Text
+    public class LocalizationText(Identifier identifier, IList<Text> args, string? languageCode = null) : Text
     {
         /// <summary>
         /// Gets the localization identifier to resolve.<br/>
         /// 해석할 로컬라이징 식별자를 가져옵니다.
         /// </summary>
-        public Identifier identifier { get; } = identifier;
+        public Identifier identifier { get; set; } = identifier;
 
         /// <summary>
         /// Gets the text arguments used by the localized format string.<br/>
         /// 로컬라이징된 format 문자열에서 사용할 텍스트 인수를 가져옵니다.
         /// </summary>
-        public IReadOnlyList<Text> args { get; } = args;
+        public IList<Text> args { get; } = args;
 
         /// <summary>
         /// Gets the language code used to resolve this translation.<br/>
