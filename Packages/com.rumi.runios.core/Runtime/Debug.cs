fa6b8ca2a88f2861dc7e7035898d3589804cac8e
Rumi <jun32820@gmail.com>
2026-06-24 08:48:25 +0900

    NameOfCallingClass 메소드 구현 수정

--- Packages/com.rumi.runios.core/Runtime/Debug.cs
+++ Packages/com.rumi.runios.core/Runtime/Debug.cs
@@ -1,13 +1,13 @@
 #nullable enable
-using RuniOS.Reflection;
 using System.Diagnostics;
 using System.Reflection;
-using System.Text.RegularExpressions;
+using System.Runtime.CompilerServices;
 
 namespace RuniOS
 {
     public static class Debug
     {
+        [MethodImpl(MethodImplOptions.NoInlining)]
         [Conditional("UNITY_EDITOR"), Conditional("UNITY_ENABLE_CHECKS")]
         public static void Log(object? message, string? className = null)
         {
@@ -15,6 +15,7 @@ namespace RuniOS
             UnityEngine.Debug.Log(GetLogText(className, message));
         }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         [Conditional("UNITY_EDITOR"), Conditional("UNITY_ENABLE_CHECKS")]
         public static void LogWarning(object? message, string? className = null)
         {
@@ -22,6 +23,7 @@ namespace RuniOS
             UnityEngine.Debug.LogWarning(GetLogText(className, message));
         }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         [Conditional("UNITY_EDITOR"), Conditional("UNITY_ENABLE_CHECKS")]
         public static void LogError(object? message, string? className = null)
         {
@@ -29,6 +31,7 @@ namespace RuniOS
             UnityEngine.Debug.LogError(GetLogText(className, message));
         }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         [Conditional("UNITY_EDITOR"), Conditional("UNITY_ENABLE_CHECKS")]
         public static void Assert(bool condition, object? message, string? className = null)
         {
@@ -36,24 +39,28 @@ namespace RuniOS
             UnityEngine.Debug.Assert(condition, GetLogText(className, message));
         }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void RuntimeLog(object? message, string? className = null)
         {
             className ??= NameOfCallingClass();
             UnityEngine.Debug.Log(GetRuntimeLogText(className, message));
         }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void RuntimeLogWarning(object? message, string? className = null)
         {
             className ??= NameOfCallingClass();
             UnityEngine.Debug.LogWarning(GetRuntimeLogText(className, message));
         }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void RuntimeLogError(object? message, string? className = null)
         {
             className ??= NameOfCallingClass();
             UnityEngine.Debug.LogError(GetRuntimeLogText(className, message));
         }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void RuntimeAssert(bool condition, object? message, string? className = null)
         {
             className ??= NameOfCallingClass();
@@ -67,54 +74,66 @@ namespace RuniOS
         // ReSharper disable Unity.PerformanceAnalysis
         public static void LogException(Exception exception) => UnityEngine.Debug.LogException(exception);
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
         public static string NameOfCallingClass(int skipFrames = 0)
         {
-            skipFrames += 2;
+            StackTrace stackTrace = new StackTrace(1, false);
 
-            StackTrace stackTrace = new StackTrace();
-            if (stackTrace.FrameCount > skipFrames)
+            for (int i = 0; i < stackTrace.FrameCount; i++)
             {
-                StackFrame stackFrame = stackTrace.GetFrame(skipFrames);
-                MethodBase methodBase = stackFrame.GetMethod();
-                Type type = methodBase.DeclaringType ?? typeof(Debug);
+                MethodBase? method = stackTrace.GetFrame(i)?.GetMethod();
+                Type? type = GetLogicalDeclaringType(method);
 
-                if (type.IsCompilerGenerated())
+                if (type == null)
+                    continue;
+
+                if (type == typeof(Debug))
+                    continue;
+
+                if (skipFrames > 0)
                 {
-                    string name = type.FullName ?? string.Empty;
-
-                    const string pattern = @"\.(.*?)\+"; 
-                    Match matches = Regex.Match(name, pattern);
-                
-                    if (matches.Groups.Count > 1)
-                        return matches.Groups[1].Value;
-                    else
-                        return name;
+                    skipFrames--;
+                    continue;
                 }
 
-                return type.Name;
+                return GetFullNameWithoutNamespace(type);
             }
-            else
-                return nameof(Debug);
+
+            return nameof(Debug);
         }
 
-        public static StackFrame GetMethodCallerStackFrame()
+        static Type? GetLogicalDeclaringType(MethodBase? method)
         {
-            StackFrame stackFrame;
-            Type? declaringType;
-            int skipFrames = 2;
-            do
+            Type? type = method?.DeclaringType;
+
+            while (type != null && IsCompilerGeneratedType(type) && type.DeclaringType != null)
             {
-                stackFrame = new StackFrame(skipFrames, true);
-                declaringType = stackFrame.GetMethod().DeclaringType;
-
-                if (declaringType == null)
-                    return stackFrame;
-
-                skipFrames++;
+                type = type.DeclaringType;
             }
-            while (declaringType.Module.Name.Equals("mscorlib.dll", StringComparison.OrdinalIgnoreCase));
 
-            return stackFrame;
+            return type;
+        }
+
+        static bool IsCompilerGeneratedType(Type type)
+        {
+            if (type.IsDefined(typeof(CompilerGeneratedAttribute), false))
+                return true;
+
+            if (typeof(IAsyncStateMachine).IsAssignableFrom(type))
+                return true;
+
+            return type.DeclaringType != null;
+        }
+
+        static string GetFullNameWithoutNamespace(Type type)
+        {
+            string fullName = type.FullName ?? type.Name;
+            string? ns = type.Namespace;
+
+            if (!string.IsNullOrEmpty(ns) && fullName.StartsWith(ns + ".", StringComparison.Ordinal))
+                return fullName.Substring(ns.Length + 1);
+
+            return fullName;
         }
     }
 }
