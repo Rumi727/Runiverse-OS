3d0faa5bee690bad7c36dfeda03c0e172ac09a8b
Rumi <jun32820@gmail.com>
2026-06-24 08:49:04 +0900

    직렬화 규칙 수정

--- Packages/com.rumi.runios.core/Runtime/Resource/AssetRef`1.cs
+++ Packages/com.rumi.runios.core/Runtime/Resource/AssetRef`1.cs
@@ -9,17 +9,17 @@ namespace RuniOS.Resource
     /// </summary>
     /// <typeparam name="TAsset">목표 리소스 타입 (예: <see cref="Texture2D"/>, <see cref="AudioClip"/>)</typeparam>
     [Serializable]
-    public record struct AssetRef<TAsset>(ResourceKey key) : IAssetRef
+    public record struct AssetRef<TAsset>(ResourceKey _key) : IAssetRef
     {
         public AssetRef(Identifier registryId, Identifier valueId) : this(new ResourceKey(registryId, valueId)) { }
 
-        [SerializeField] public ResourceKey key = key;
-
         Type IAssetRef.targetAssetType => typeof(TAsset);
-        ResourceKey IAssetRef.key { get => key; set => key = value; }
+        public ResourceKey key { get => _key; set => _key = value; }
 
-        public readonly void Deconstruct(out Identifier registryId, out Identifier assetId) => key.Deconstruct(out registryId, out assetId);
+        [SerializeField] ResourceKey _key = _key;
 
-        public async UniTask<IAssetScope<TAsset>?> LoadAsync() => await ResourceManager.LoadScopeAsync<TAsset>(key);
+        public readonly void Deconstruct(out Identifier registryId, out Identifier assetId) => _key.Deconstruct(out registryId, out assetId);
+
+        public async UniTask<IAssetScope<TAsset>?> LoadAsync() => await ResourceManager.LoadScopeAsync<TAsset>(_key);
     }
 }
