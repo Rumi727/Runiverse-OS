e876640ea2e849ca4bb9fe1415eec15c4cd886f3
Rumi <jun32820@gmail.com>
2026-07-30 10:23:06 +0900

    Create CharPropertyConverter.cs

---
+++ Packages/com.rumi.runios.core/Editor/Unity/Serialization/Converters/CharPropertyConverter.cs
@@ -0,0 +1,10 @@
+﻿#nullable enable
+namespace RuniOS.Editor.Unity.Serialization.Converters
+{
+    [CustomPropertyConverter(typeof(char))]
+    public class CharPropertyConverter : PropertyConverter
+    {
+        public override object Read(SerializedProperty property, Type propertyType) => (char)property.intValue;
+        public override void Write(SerializedProperty property, Type propertyType, object? value) => property.intValue = (char)(value ?? 0);
+    }
+}
