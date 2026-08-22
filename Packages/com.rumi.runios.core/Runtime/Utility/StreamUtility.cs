07af28091cd4ca55220a2da39cbe0df10c1e1625
Rumi <jun32820@gmail.com>
2026-08-22 22:28:46 +0900

    Update StreamUtility.cs

--- Packages/com.rumi.runios.core/Runtime/Utility/StreamUtility.cs
+++ Packages/com.rumi.runios.core/Runtime/Utility/StreamUtility.cs
@@ -12,27 +12,6 @@ namespace RuniOS.Utility
             if (stream is MemoryStream memoryStream)
                 return memoryStream.ToArray();
 
-            if (stream.CanSeek)
-            {
-                long length = stream.Length - stream.Position;
-                if (length == 0)
-                    return [];
-
-                byte[] buffer = new byte[length];
-                int totalBytesRead = 0;
-
-                while (totalBytesRead < buffer.Length)
-                {
-                    int bytesRead = stream.Read(buffer, totalBytesRead, buffer.Length - totalBytesRead);
-                    if (bytesRead == 0)
-                        throw new EndOfStreamException();
-
-                    totalBytesRead += bytesRead;
-                }
-
-                return buffer;
-            }
-
             using MemoryStream result = new MemoryStream();
             stream.CopyTo(result, 81920);
             return result.ToArray();
@@ -42,27 +21,6 @@ namespace RuniOS.Utility
         {
             if (stream is MemoryStream memoryStream)
                 return memoryStream.ToArray();
-
-            if (stream.CanSeek)
-            {
-                long length = stream.Length - stream.Position;
-                if (length == 0)
-                    return [];
-
-                byte[] buffer = new byte[length];
-                int totalBytesRead = 0;
-
-                while (totalBytesRead < buffer.Length)
-                {
-                    int bytesRead = await stream.ReadAsync(buffer, totalBytesRead, buffer.Length - totalBytesRead, cancellationToken);
-                    if (bytesRead == 0)
-                        throw new EndOfStreamException();
-
-                    totalBytesRead += bytesRead;
-                }
-
-                return buffer;
-            }
 
             using MemoryStream result = new MemoryStream();
             await stream.CopyToAsync(result, 81920, cancellationToken);
