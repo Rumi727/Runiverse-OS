d88611391c47ae1e498a556bc3133399a910c6e6
Rumi <jun32820@gmail.com>
2026-08-29 23:12:40 +0900

    Update TypeInspectorDrawer.cs

--- Packages/com.rumi.runios.core/Editor/Inspectors/Drawers/IMGUI/TypeInspectorDrawer.cs
+++ Packages/com.rumi.runios.core/Editor/Inspectors/Drawers/IMGUI/TypeInspectorDrawer.cs
@@ -7,7 +7,7 @@ using RuniOS.Undos;
 
 namespace RuniOS.Editor.Inspectors.Drawers.IMGUI
 {
-    [InspectorDrawer(typeof(Type))]
+    [InspectorDrawer(typeof(Type), true)]
     public class TypeInspectorDrawer(IInspectorVariableElement element, IEnumerable<IInspectorAttribute> inheritedAttributes, IUndoRecorder? undoRecorder = null) : GenericInspectorDrawer(element, inheritedAttributes, undoRecorder)
     {
         protected override object? DrawField(Rect position, GUIContent label, object? value, DrawerContext context = default) => RuniFields.TypeField(position, label, (Type?)value);
